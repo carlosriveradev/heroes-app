53597a4a136d0ef5d155efe15a7f693f620dc6af
Carlos Rivera <carlosrivera.dev@gmail.com>
2022-12-26 10:47:23 -0500

    try 2 fix error

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -17,7 +17,7 @@ export const HeroCard = ({
     characters,
 }) => {
 
-    const heroImageUrl = `/heroes/${id}.jpg`
+    const heroImageUrl = `./heroes/${id}.jpg`;
 
     // const charactesByHero = (<p>{characters}</p>)
 
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -12,37 +12,44 @@ export const HeroPage = () => {
         navigate(-1)
     }
 
-    if (!hero){
-        return <Navigate to="/marvel" />
-    }
+    const heroImageUrl = `./heroes/${id}.jpg`;
 
-    return (
-        <div className="row mt-5">
-            <div className="col-4">
-                <img 
-                src={`/assets/heroes/${id}.jpg`} 
-                alt={hero.superhero} 
-                className="img-thumbnail animate__animated animate__fadeInLeft"
-                />
-            </div>
-            <div className="col-8">
-                <h3>{hero.superhero}</h3>
-                <ul className="list-group list-group-flush">
-                    <li className="list-item"><b>Alter ego:</b> {hero.alter_ego}</li>
-                    <li className="list-item"><b>Publisher:</b> {hero.publisher}</li>
-                    <li className="list-item"><b>First Appearance:</b> {hero.first_appearance}</li>
-                </ul>
+	if (!hero) {
+		return <Navigate to='/marvel' />;
+	}
 
-                <h5 className="mt3">Characters</h5>
-                <p>{hero.characters}</p>
+	return (
+		<div className='row mt-5'>
+			<div className='col-4'>
+				<img
+					src={heroImageUrl}
+					alt={hero.superhero}
+					className='img-thumbnail animate__animated animate__fadeInLeft'
+				/>
+			</div>
+			<div className='col-8'>
+				<h3>{hero.superhero}</h3>
+				<ul className='list-group list-group-flush'>
+					<li className='list-item'>
+						<b>Alter ego:</b> {hero.alter_ego}
+					</li>
+					<li className='list-item'>
+						<b>Publisher:</b> {hero.publisher}
+					</li>
+					<li className='list-item'>
+						<b>First Appearance:</b> {hero.first_appearance}
+					</li>
+				</ul>
 
-                <button 
-                    className="btn btn-outline-primary"
-                    onClick={onNavigateBack}
-                >
-                    Regresar
-                </button>
-            </div>
-        </div>
-    )
+				<h5 className='mt3'>Characters</h5>
+				<p>{hero.characters}</p>
+
+				<button
+					className='btn btn-outline-primary'
+					onClick={onNavigateBack}>
+					Regresar
+				</button>
+			</div>
+		</div>
+	);
 }
